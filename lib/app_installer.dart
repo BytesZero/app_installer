16b3d47a675d2fe8c02588f77975d4e06db874c4
Zero <1300326388@qq.com>
2019-08-06 22:55:14 +0800

    1、增加规范

--- lib/app_installer.dart
+++ lib/app_installer.dart
@@ -5,17 +5,20 @@ import 'package:flutter/services.dart';
 class AppInstaller {
   static const MethodChannel _channel = const MethodChannel('app_installer');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
   ///去应用商店
   ///
   static Future<void> goStore(String androidAppId, String iOSAppId,
-      {bool review = false}) async {}
+      {bool review = false}) async {
+    _channel.invokeMethod('goStore', {
+      'androidAppId': androidAppId,
+      'iOSAppId': iOSAppId,
+      'review': review,
+    });
+  }
 
   ///安装 Apk
   ///[filePath] Apk file path
-  static Future<void> installApk(String filePath) async {}
+  static Future<void> installApk(String filePath) async {
+    _channel.invokeMethod('installApk', {'filePath': filePath});
+  }
 }
